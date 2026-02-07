4bc7f0773efe021761710204703a5a60867bfe53
Gelsi Brito <gelsitoguzman@gmail.com>
2026-02-07 18:35:05 -0400

    subiendo los ejercicios realizados de destruturacion

--- Basic/21-destructuring-spreading-exercises.js
+++ Basic/21-destructuring-spreading-exercises.js
@@ -3,23 +3,149 @@ Clase 36 - Ejercicios: Desestructuración y propagación
 Vídeo: https://youtu.be/1glVfFxj8a4?t=16802
 */
 
-// 1. Usa desestructuración para extraer los dos primeros elementos de un array 
+/** 
+ * ! 1. Usa desestructuración para extraer los dos primeros elementos de un array 
+*/
+// let miArray = ["perro", "gato", "leon", "tigre", "elefante"]
 
-// 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
+// let [element1, element2] = miArray
+// console.log(element1)
+// console.log(element2)
+// console.log("------------------------------------")
 
-// 3. Usa desestructuración para extraer dos propiedades de un objeto
+/** 
+ * ! 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
+*/
+// let miArray = ["perro", "gato", "leon", "tigre", "elefante"]
 
-// 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
-//    a nuevas variables con nombres diferentes
+// let [element1, element2, element3 = "vacio"] = miArray
 
-// 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+// console.log(element1)
+// console.log(element2)
+// console.log(element3)
+// console.log("------------------------------------")
 
-// 6. Usa propagación para combinar dos arrays en uno nuevo
+/** 
+ * ! 3. Usa desestructuración para extraer dos propiedades de un objeto
+*/
 
-// 7. Usa propagación para crear una copia de un array
+// let person = {
+//     nombre: "Gelsi",
+//     edad: 24,
+//     carrera: "informatica"
+// }
 
-// 8. Usa propagación para combinar dos objetos en uno nuevo
+// let {nombre , edad} = person
 
-// 9. Usa propagación para crear una copia de un objeto
+// console.log(nombre)
+// console.log(edad)
+// console.log("------------------------------------")
 
-// 10. Combina desestructuración y propagación
+
+/** 
+ * ! 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
+ * ! a nuevas variables con nombres diferentes
+*/
+// let person1 = {
+//     nombre: "Gelsi",
+//     edad: 24,
+//     carrera: "informatica"
+// }
+
+// let {nombre: propiedad1 , edad: propiedad2} = person1
+
+// console.log(propiedad1)
+// console.log(propiedad2)
+
+// console.log("------------------------------------")
+
+/** 
+ * ! 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+*/
+// let car = {
+//     year: "2012",
+//     enginer: "v4",
+//     doors: 4,
+//     sport: {
+//         enginer: "turbo v6",
+//         doors: 2
+//     }
+// }
+
+// let {sport:{enginer, doors}} = car
+
+// console.log(enginer)
+// console.log(doors)
+
+
+/** 
+ * ! 6. Usa propagación para combinar dos arrays en uno nuevo
+*/
+// let miArray1 = ["perro", "gato", "leon", "tigre", "elefante"]
+// let miarray2 = ["dinosaurio", "king kong"]
+
+// let myNewArray = [...miArray1, ...miarray2]
+// console.log(myNewArray)
+/** 
+ * ! 7. Usa propagación para crear una copia de un array
+*/
+// let miArray1 = ["perro", "gato", "leon", "tigre", "elefante"]
+// let myNewArray = [...miArray1]
+// console.log(myNewArray)
+
+/** 
+ * ! 8. Usa propagación para combinar dos objetos en uno nuevo
+*/
+// let person2 = {
+//     nombre: "Pedro",
+//     edad: 22,
+//     carrera: "informatica",
+//     experiencia: 3,
+//     puesto: "Desarrollo de software"
+// }
+// let person1 = {
+//     nombre: "Gelsi",
+//     edad: 24,
+//     carrera: "informatica",
+//     puesto: "CiberSecurity"
+// }
+
+// let newObject = {...person2, ...person1}
+
+// console.log(newObject)
+
+/** 
+ * ! 9. Usa propagación para crear una copia de un objeto
+*/
+// let person1 = {
+//     nombre: "Gelsi",
+//     edad: 24,
+//     carrera: "informatica",
+//     puesto: "CiberSecurity"
+// }
+
+// let newObject = {...person1}
+
+// console.log(newObject)
+
+/** 
+ * ! 10. Combina desestructuración y propagación
+*/
+let person2 = {
+    nombre: "Pedro",
+    edad: 22,
+    carrera: "informatica",
+    experiencia: 3,
+    puesto: "Desarrollo de software"
+}
+let person1 = {
+    nombre: "Gelsi",
+    edad: 24,
+    carrera: "informatica",
+    puesto: "CiberSecurity"
+}
+
+let {edad, ...newObject} = person1
+
+console.log(edad)
+console.log(newObject)
