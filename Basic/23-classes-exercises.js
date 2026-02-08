290f4536c7663558785f744974dc34d69e8a7049
Gelsi Brito <gelsitoguzman@gmail.com>
2026-02-07 20:14:11 -0400

    subiendo los ejercicios de clases

--- Basic/23-classes-exercises.js
+++ Basic/23-classes-exercises.js
@@ -3,22 +3,300 @@ Clase 39 - Ejercicios: Clases
 Vídeo: https://youtu.be/1glVfFxj8a4?t=18630
 */
 
-// 1. Crea una clase que reciba dos propiedades
+/** 
+   * !1. Crea una clase que reciba dos propiedades
+*/
 
-// 2. Añade un método a la clase que utilice las propiedades
+// class Person {
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
+// }
 
-// 3. Muestra los valores de las propiedades e invoca a la función
+/** 
+   * !2. Añade un método a la clase que utilice las propiedades
+*/
 
-// 4. Añade un método estático a la primera clase
+// class Person {
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
 
-// 5. Haz uso del método estático
+//     text(){
+//         console.log(`My name is ${this.name} and i have ${this.age} ages!`)
+//     }
+// }
 
-// 6. Crea una clase que haga uso de herencia
+/** 
+   * !3. Muestra los valores de las propiedades e invoca a la función
+*/
 
-// 7. Crea una clase que haga uso de getters y setters
 
-// 8. Modifica la clase con getters y setters para que use propiedades privadas
+// class Person {
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
 
-// 9. Utiliza los get y set y muestra sus valores
+//     text(){
+//         console.log(`My name is ${this.name} and I have ${this.age} ages!`)
+//     }
+// }
+// let person1 = new Person("Gelsi", 24)
 
-// 10. Sobrescribe un método de una clase que utilice herencia 
+
+// console.log(person1.name)
+// console.log(person1.age)
+// person1.text()
+
+
+/** 
+   * !4. Añade un método estático a la primera clase
+*/
+
+// class Person {
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
+
+//     text(){
+//         console.log(`My name is ${this.name} and I have ${this.age} ages!`)
+//     }
+//     static information(){
+//         console.log("I am a person!!!")
+//     }
+// }
+
+// let person1 = new Person("Gelsi", 24)
+
+// console.log(person1.name)
+// console.log(person1.age)
+// person1.text()
+
+
+/** 
+   * !5. Haz uso del método estático
+*/
+
+// class Person {
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
+
+//     text(){
+//         console.log(`My name is ${this.name} and I have ${this.age} ages!`)
+//     }
+//     static information(){
+//         console.log("I am a person!!!")
+//     }
+// }
+
+// let person1 = new Person("Gelsi", 24)
+
+// console.log(person1.name)
+// console.log(person1.age)
+// person1.text()
+// Person.information()
+
+
+/** 
+   * !6. Crea una clase que haga uso de herencia
+*/
+
+// class Person {
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
+
+//     text(){
+//         console.log(`My name is ${this.name} and I have ${this.age} ages!`)
+//     }
+//     static information(){
+//         console.log("I am a person!!!")
+//     }
+// }
+
+// let person1 = new Person("Gelsi", 24)
+
+// console.log(person1.name)
+// console.log(person1.age)
+// person1.text()
+// Person.information()
+
+
+// class Empleado extends Person {
+
+// }
+
+// let empleado1 = new Empleado("Pedro", 44)
+// console.log(empleado1.name)
+// console.log(empleado1.age)
+// empleado1.text()
+// Empleado.information()
+
+
+/** 
+   * !7. Crea una clase que haga uso de getters y setters
+*/
+
+
+// class Person {
+
+//     #name
+//     #age
+
+//     constructor(name = "pablito", age = 20){
+//         this.#name = name
+//         this.#age = age
+//     }
+
+//     get name (){
+//         return this.#name
+//     }
+//     set age (age){
+//         this.#age = age
+//     }
+//     get age (){
+//         return this.#age
+//     }
+
+//     text(){
+//         console.log(`My name is ${this.name} and I have ${this.age} ages!`)
+//     }
+//     static information(){
+//         console.log("I am a person!!!")
+//     }
+// }
+
+// let person1 = new Person("Gelsi", 24)
+
+// console.log(person1.name)
+// console.log(person1.age)
+// person1.text()
+// Person.information()
+
+
+/** 
+   * !8. Modifica la clase con getters y setters para que use propiedades privadas
+*/
+
+// class Person {
+
+//     #name
+//     #age
+
+//     constructor(name = "pablito", age = 20){
+//         this.#name = name
+//         this.#age = age
+//     }
+
+//     get name (){
+//         return this.#name
+//     }
+//     set age (age){
+//         this.#age = age
+//     }
+//     get age (){
+//         return this.#age
+//     }
+
+//     text(){
+//         console.log(`My name is ${this.name} and I have ${this.age} ages!`)
+//     }
+//     static information(){
+//         console.log("I am a person!!!")
+//     }
+// }
+
+// let person1 = new Person("Gelsi", 24)
+
+// console.log(person1.name)
+// console.log(person1.age)
+// person1.text()
+// Person.information()
+
+
+/** 
+   * !9. Utiliza los get y set y muestra sus valores
+*/
+
+// class Person {
+
+//     #name
+//     #age
+
+//     constructor(name = "pablito", age = 20){
+//         this.#name = name
+//         this.#age = age
+//     }
+
+//     get name (){
+//         return this.#name
+//     }
+//     set age (age){
+//         this.#age = age
+//     }
+//     get age (){
+//         return this.#age
+//     }
+
+//     text(){
+//         console.log(`My name is ${this.name} and I have ${this.age} ages!`)
+//     }
+//     static information(){
+//         console.log("I am a person!!!")
+//     }
+// }
+
+// let person1 = new Person("Gelsi", 24)
+
+// console.log(person1.name)
+// console.log(person1.age)
+// person1.age = 25
+// person1.text()
+// Person.information()
+
+
+/** 
+   * !10. Sobrescribe un método de una clase que utilice herencia 
+*/
+
+// class Person {
+//     constructor(name, age){
+//         this.name = name
+//         this.age = age
+//     }
+
+//     text(){
+//         console.log(`My name is ${this.name} and I have ${this.age} ages!`)
+//     }
+//     static information(){
+//         console.log("I am a person!!!")
+//     }
+// }
+
+// let person1 = new Person("Gelsi", 24)
+
+// console.log(person1.name)
+// console.log(person1.age)
+// person1.text()
+// Person.information()
+
+
+// class Empleado extends Person {
+
+//     text() {
+//         console.log(`My name is ${this.name}, I have ${this.age} ages and I am employer!`)
+//     }
+// }
+
+// let empleado1 = new Empleado("Pedro", 44)
+// console.log(empleado1.name)
+// console.log(empleado1.age)
+// empleado1.text()
+// Empleado.information()
